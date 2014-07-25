29b3839fe44e41658cb4dd5d305586579c869f4f
Student <student@gir.(none)>
2014-07-24 21:30:18 -0400

    words. i added IPs

--- rosinit.bash
+++ rosinit.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export ROS_MASTER_URI=http://jump.cs.pitt.edu:11311
-export ROS_IP=150.212.81.7
+#export ROS_IP=150.212.81.7  rosie	
+#export ROS_IP=150.212.81.10 gir
+#export ROS_IP=150.212.81.4  gort
 source devel/setup.bash
 
