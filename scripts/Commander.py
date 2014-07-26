a130db6ba046442caa3b9ef9588896f1c2fdca70
rab170 <rab170@pitt.edu>
2014-07-26 19:05:35 -0400

    added/fixed/modified/etc potential function

--- scripts/Commander.py
+++ scripts/Commander.py
@@ -12,12 +12,10 @@ class Commander(object):
         super(Commander, self).__init__()
         # array of robot data
         self.robots = [Robot(Robot.HUMAN), Robot(Robot.HUMAN), Robot(Robot.HUMAN)]
-        
         rospy.init_node('Commander', anonymous=True)
         rospy.Service('potential_field', PotentialField, self.calc_potential)
         rospy.Subscriber('/robots/location', LocationList, self.cache_locations)
-        self.bump_sub = rospy.Subscriber('/robots/bumps', Bump, self.handle_bump)     
-        
+        self.bump_sub = rospy.Subscriber('/robots/bumps', Bump, self.handle_bump)
 
     def calc_potential(self, request):
         # robot that requested potential
@@ -29,9 +27,10 @@ class Commander(object):
             potentials.append(robot.get_potential(other_robot))
 
         # take list of 2-element tuples and add them
-        potential = reduce(lambda x, y: (x[0]+y[0], x[1]+y[1]), potentials) 
-        
-        return PotentialFieldResponse(potential)
+        potential = reduce(lambda x, y: (x[0]+y[0], x[1]+y[1]), potentials)
+        norm = math.sqrt(potential[0]**2 + potential[1]**2)
+        normalized_potential = tuple(val/norm for val in potential)
+        return PotentialFieldResponse(normalized_potential)
 
     def handle_bump(self, msg):
         robot = self.robots[msg.robotID]
@@ -56,22 +55,30 @@ class Robot(object):
         if self.status is ZOMBIE and other.status is HUMAN:
             other.status = ZOMBIE
 
+    def get_potential(self):
+        potential_vector = numpy.array([0,0])
+        if other.ID is self.ID:
+            continue
+        potential_vector=numpy.add(potential_vector, force_vector)
+        normal_potential = potential_vector/numpy.linalg.norm(potential_vector)
+
+
     def get_potential(self, other):
         # same robot, no repulsion
         if self is other:
-            pass
-        # same status, slight repulsion
-        elif self.status is other.status:
-            pass
-        # zombie chases human, strong attraction
-        elif self.status is ZOMBIE and other.status is HUMAN:
-            pass
-        # human runs from zombie, strong repulsion
-        else:
-            pass
-        # todo: actually calculate potential
-        return (0,0)
-            
+            return (0,0)
+        dx = self.getX() - other.getX()
+        dy = self.getY() - other.getY()
+        r = numpy.array([dx, dy])
+        r_hat = r/numpy.linalg.norm(direction)
+        force = 1/numpy.linalg.norm(r)**2
+        force_vector = force*r_hat
+        if self.status == self.ZOMBIE and robot.status == self.HUMAN:
+            force = -1*force                    #By default, this vector will repel. But zombies must pursue humans.
+        if self.status == other.status:
+            pass                                #Consider scaling down the effect of like-robots
+        return (force_vector[0], force_vector[1])
+
 if __name__ == "__main__":
     try:
         cmdr = Commander()
