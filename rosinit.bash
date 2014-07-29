487e43cf89eaf45dc97790341fb56298d3296f7d
Student <student@gort.(none)>
2014-07-28 22:27:00 -0400

    use ROS_IP

--- rosinit.bash
+++ rosinit.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+
 export ROS_MASTER_URI="http://$1.cs.pitt.edu:11311"
-#export ROS_IP=150.212.81.7
-export ROS_HOSTNAME="$2.cs.pitt.edu"
+export ROS_IP=$(/sbin/ifconfig wlan0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
 source devel/setup.bash
 
